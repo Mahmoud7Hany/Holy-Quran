9989d375a17964bb210544f2968e61704ff29bbc
Mahmoud7Hany <rh2811100@gmail.com>
2023-01-06 18:42:31 +0200

    هنا الرساله

---
+++ lib/arabic_surah_number.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:quran_kareem/arabic_number_converter.dart';
+
+class ArabicSurahNumber extends StatelessWidget {
+  const ArabicSurahNumber({Key? key, required this.i}) : super(key: key);
+//  صفحه رقم السورة العربية الي هو مكان رقم الايه الي هيا العلامه الفاصل بين الايات
+  final int i;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "\uFD3E${(i + 1).toString().toArabicNumbers}\uFD3F",
+      style: const TextStyle(
+        color: Color.fromARGB(255, 0, 0, 0),
+        fontFamily: "me_quran",
+        fontSize: 20.0,
+        shadows: [
+          Shadow(
+            color: Colors.amberAccent,
+            offset: Offset(0.5, 0.5),
+            blurRadius: 1.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/my_drawer.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:quran_kareem/constant.dart';
+import 'package:quran_kareem/settings.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
+// Drawer دي صفحه
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: const BoxDecoration(color: Colors.white),
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/images/quran.png",
+                  height: 80.0,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "القرآن الكريم",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  textDirection: TextDirection.rtl,
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text(
+              "إعدادات",
+              style: TextStyle(fontSize: 20),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SettingsScreen(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.share),
+            title: const Text(
+              "شارك التطبيق",
+              style: TextStyle(fontSize: 20),
+            ),
+            onTap: () {
+              Share.share("تطبيق القرآن الكريم مجانا و بدون انترنت \n $urlApp");
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.rate_review),
+            title: const Text(
+              "قيم التطبيق",
+              style: TextStyle(fontSize: 20),
+            ),
+            onTap: () async {
+              if (!await launchUrl(urlApp,
+                  //  السطر ده علشان يفتح الرابط خارج التطبيق
+                  mode: LaunchMode.externalApplication)) {
+                throw 'تعذر الإطلاق $urlApp';
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
